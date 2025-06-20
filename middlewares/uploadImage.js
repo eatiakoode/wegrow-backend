c6169a8020e590f9c5f72a25207ae57816addc4a
eatiakoode <eati@akoode.in>
2025-06-20 18:47:08 +0530

    image

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -230,7 +230,7 @@ const propertySelectedImgsResize = async (req) => {
         .jpeg({ quality: 90 })
         .toFile(outputPath);
 
-      fs.unlinkSync(file.path); // delete original uploaded file
+      // fs.unlinkSync(file.path); // delete original uploaded file
 
       processedFilenames.push("public/images/propertyimage/"+filename);
     })
